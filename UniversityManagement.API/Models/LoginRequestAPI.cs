2d9b54f609821504e0f9e7a59476620d3858d4b4
BaoSon <baoson20022001@gmail.com>
2023-11-27 22:29:54 +0700

    commit

---
+++ UniversityManagement.API/Models/LoginRequestAPI.cs
@@ -0,0 +1,8 @@
+﻿namespace UniversityManagement.API.Models
+{
+    public class LoginRequestAPI
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
